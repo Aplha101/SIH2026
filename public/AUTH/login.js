9a24b6b6e4d15475e05b33ba37ddef52b7ea673e
Aplha101 <dhruvbeastboy2020@gmail.com>
2026-08-17 09:11:57 +0530

    login

---
+++ public/AUTH/login.js
@@ -0,0 +1,9 @@
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const login = document.getElementById("login")
+let logged = false
+
+
+login.addEventListener("click" , (e) => {
+    
+})
